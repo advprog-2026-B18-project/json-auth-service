01de47102a54402dba6565575957cc589290207e
r3gulus-4rcturus <muh.lanangzalkifla@gmail.com>
2026-02-19 16:58:50 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/jsonauthservice/JsonAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.jsonauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonAuthServiceApplication.class, args);
+    }
+
+}
